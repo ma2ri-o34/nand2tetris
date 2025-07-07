861634a2d43344d1b40829342143017a08a70b8c
ma2ri0 <84706241+ma2ri-o34@users.noreply.github.com>
2025-07-08 00:00:02 +0900

    add: fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,42 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+(LOOP)
+  @i
+  M=0
+  @24576 // 押下されたキーの判別ワード
+  D=M
+  @NOTFILL
+  D;JEQ  // M[24576]のいずれのbitも立っていないならJUMP
+(FILL) // 
+  @i
+  D=M
+  @8192 // スクリーンサイズは512 * 256 
+  D=D-A 
+  @LOOP
+  D;JEQ // for(i=0, i=>8192, i++)
+  @SCREEN 
+  D=A
+  @i
+  A=D+M
+  M=-1 //0xffffで1ワードすべて塗りつぶし
+  @i
+  M=M+1
+  @FILL
+  0;JMP
+(NOTFILL)
+  @i
+  D=M 
+  @8192
+  D=D-A
+  @LOOP
+  D;JEQ // for(i=0, i=>8192, i++)
+  @SCREEN
+  D=A
+  @i
+  A=D+M
+  M=0 //0x0000で1ワードすべて記述しない
+  @i
+  M=M+1
+  @NOTFILL
+  0;JMP
